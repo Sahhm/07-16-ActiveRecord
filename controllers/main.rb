823a807d1d6f7d0472d4dfa4ca4a2afeeb31f89d
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-16 16:21:08 -0500

    update photo functional

--- controllers/main.rb
+++ controllers/main.rb
@@ -12,7 +12,7 @@ end
 
 get "/add_photo" do
   a = Photo.create(title: params["title"], location: params["location"], date: params["date"].to_i, url: params["url"], photographer_id: params["photographer_id"].to_i)
-  b = Album.find(params["album_id"])
+  b = Album.find(params["album_id"].to_i)
   b.photos << a
   
   erb :"add_photo"
@@ -24,10 +24,41 @@ get "/add_to_another_album_form" do
 end
 
 get "/add_to_another_album" do
-  a = Photo.find(params["photo_id"])
-  b = Album.find(params["album_id"])
+  a = Photo.find(params["photo_id"].to_i)
+  b = Album.find(params["album_id"].to_i)
   
   b.photos << a
   
   erb :"add_to_another_album"
 end
+
+get "/update_photo_form" do
+  erb :"update_photo_form"
+end
+
+get "/update_photo" do
+  a = Photo.find(params["photo_id"].to_i)
+  if params["title"] != ""
+    a.update(title: params["title"])
+  end
+  if params["location"] != ""
+    a.update(location: params["location"])
+  end
+  if params["date"] != ""
+    a.update(date: params["date"].to_i)
+  end
+  if params["url"] != ""
+    a.update(url: params["url"])
+  end
+  if params["photographer_id"] != ""
+    a.update(photographer_id: params["photographer_id"].to_i)
+  end
+  a.save
+  erb :"update_photo"
+end
+  
+  
+  
+  
+  
+  
