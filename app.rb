f36136d61c14697606f47a4275cbdf19e75866ab
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-17 10:57:16 -0500

    add production database settings

--- app.rb
+++ app.rb
@@ -21,6 +21,20 @@ require_relative "models/album.rb"
 
 require_relative "controllers/main.rb"
 
+configure :production do  
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+
 unless ActiveRecord::Base.connection.table_exists?(:photographers)
   ActiveRecord::Base.connection.create_table :photographers do |t|
     t.string :name
