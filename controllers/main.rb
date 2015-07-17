6688862feddb2709863471b9e5dbc53e063ceb1e
Sam Morgan <samorgan@huskers.unl.edu>
2015-07-17 15:15:35 -0500

    deleting photographer also deletes all photos associated w/ him

--- controllers/main.rb
+++ controllers/main.rb
@@ -66,6 +66,46 @@ get "/add_album" do
   Album.create(subject: params["subject"])
   erb :"add_album"
 end
+
+get "/add_photographer_form" do
+  erb :"add_photographer_form"
+end
+
+get "/add_photographer" do
+  Photographer.create(name: params["name"], hometown: params["hometown"])
+  erb :"add_photographer"
+end
   
+get "/delete_photo_form" do
+  erb :"delete_photo_form"
+end
+
+get "/delete_photo" do
+  Photo.create(name: params["name"], hometown: params["hometown"])
+  erb :"delete_photo"
+end
+
+get "/delete_album_form" do
+  erb :"delete_album_form"
+end
+
+get "/delete_album" do
+  Album.create(name: params["name"], hometown: params["hometown"])
+  erb :"delete_album"
+end  
   
-  
+get "/delete_photographer_form" do
+  erb :"delete_photographer_form"
+end
+
+get "/delete_photographer" do
+  @b = params["photographer_id"]
+  erb :"delete_photographer"
+end  
+
+get "/confirm_delete" do
+  u = Photographer.find(params["photographer_id"])
+  u.photos.destroy_all
+  u.delete
+  erb :"homepage"
+end
